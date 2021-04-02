078996f053b1f8618fc0f14e292576d66b549129
Eugene Molotov <molotov@it-projects.info>
2021-04-02 13:58:47 +0500

    saved

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@ docker-compose run --rm \
                odoo odoo \
                -d sye_test \
                -i sye_dadata \
+               --log-handler=odoo.addons.sye_dadata:DEBUG \
                --log-handler=odoo.addons.sye_sync_kis:DEBUG \
                --test-enable --stop-after-init
